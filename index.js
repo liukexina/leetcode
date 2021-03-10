8f3d9e957b084e6de59d3c72f261193ee33d23f0
刘可新 <liukexin08@maoyan.com>
2021-03-10 20:21:53 +0800

    合并两个排序的链表

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var reverseList = function (head) {
-	if (head === null || head.next === null) {
-		return head;
-	}
+var mergeTwoLists = function(l1, l2) {
+  let head = new ListNode(0);
+  const h = head;
+  while(l1 !== null && l2 !== null) {
+    if(l1.val <= l2.val) {
+      head.next = l1
+      l1 = l1.next
+    } else {
+        head.next = l2
+        l2 = l2.next
+    }
+    head = head.next;
+  }
 
-	const ret = reverseList(head.next);
-	head.next.next = head;
-	head.next = null;
-
-	return ret;
+  if(l1 !== null) {
+    head.next = l1
+  } else if(l2 !== null) {
+    head.next = l2
+  }
+  return h.next;
 };
