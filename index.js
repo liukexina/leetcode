7608d2cfddc7bdc73096d6559ee1cc984dacace9
刘可新 <liukexin08@maoyan.com>
2021-03-10 20:36:22 +0800

    二叉树的镜像

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-var mergeTwoLists = function(l1, l2) {
-  let head = new ListNode(0);
-  const h = head;
-  while(l1 !== null && l2 !== null) {
-    if(l1.val <= l2.val) {
-      head.next = l1
-      l1 = l1.next
-    } else {
-        head.next = l2
-        l2 = l2.next
-    }
-    head = head.next;
-  }
-
-  if(l1 !== null) {
-    head.next = l1
-  } else if(l2 !== null) {
-    head.next = l2
-  }
-  return h.next;
+var mirrorTree = function (root) {
+	if (root === null) {
+		return null;
+	}
+	if (root.left !== null || root.right !== null) {
+		const r = root.left;
+		root.left = root.right;
+		root.right = r;
+		mirrorTree(root.left);
+		mirrorTree(root.right);
+	}
+	return root;
 };
