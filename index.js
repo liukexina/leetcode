8b0c57b807100e2f26da77508042c1eacd801927
刘可新 <liukexin08@maoyan.com>
2021-03-18 11:43:25 +0800

    从上到下打印二叉树

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var mirrorTree = function (root) {
-	if (root === null) {
-		return null;
+function levelOrder(root) {
+	const link = [];
+	const queue = [];
+	if(root != null) queue.push(root);
+	while(queue.length !== 0) {
+			const arr = []
+			for(let i = queue.length ; i > 0 ; i--) {
+					const node = queue.shift();
+					arr.push(node.val)
+					if(node.left) queue.push(node.left)
+					if(node.right) queue.push(node.right);
+			}
+			link.push(arr);
 	}
-	if (root.left !== null || root.right !== null) {
-		const r = root.left;
-		root.left = root.right;
-		root.right = r;
-		mirrorTree(root.left);
-		mirrorTree(root.right);
-	}
-	return root;
-};
+	return link;
+}
