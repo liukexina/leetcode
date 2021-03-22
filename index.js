37aaab5ad78035b85d5c38a1ccac6de1c9a30031
刘可新 <liukexin08@maoyan.com>
2021-03-22 20:36:33 +0800

    顺时针打印矩阵

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function levelOrder(root) {
-	const link = [];
-	const queue = [];
-	if(root != null) queue.push(root);
-	while(queue.length !== 0) {
-			const arr = []
-			for(let i = queue.length ; i > 0 ; i--) {
-					const node = queue.shift();
-					arr.push(node.val)
-					if(node.left) queue.push(node.left)
-					if(node.right) queue.push(node.right);
-			}
-			link.push(arr);
+var spiralOrder = function(matrix) {
+	if (!matrix.length || !matrix[0].length) {
+			return [];
 	}
-	return link;
-}
+
+	const rows = matrix.length, columns = matrix[0].length;
+	const order = [];
+	let left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+	while (left <= right && top <= bottom) {
+			for (let column = left; column <= right; column++) {
+					order.push(matrix[top][column]);
+			}
+			for (let row = top + 1; row <= bottom; row++) {
+					order.push(matrix[row][right]);
+			}
+			if (left < right && top < bottom) {
+					for (let column = right - 1; column > left; column--) {
+							order.push(matrix[bottom][column]);
+					}
+					for (let row = bottom; row > top; row--) {
+							order.push(matrix[row][left]);
+					}
+			}
+			[left, right, top, bottom] = [left + 1, right - 1, top + 1, bottom - 1];
+	}
+	return order;
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/shun-shi-zhen-da-yin-ju-zhen-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
