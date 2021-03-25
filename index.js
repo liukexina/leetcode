d2d1514dafdb0c031d949feea7da45a1f283bbb8
刘可新 <liukexin08@maoyan.com>
2021-03-25 10:29:13 +0800

    两个链表的第一个公共节点

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var firstUniqChar = function (s) {
-	let arr = [];
-	for (let i = 0; i < s.length; i++) {
-		const str = s.substring(i + 1);
-		if (arr.indexOf(s[i]) === -1) {
-			if (str === '' || !str.includes(s[i])) {
-				return s[i];
-			} else {
-				arr.push(s[i]);
-			}
-		}
+	let map = new Map();
+	for (const c of s) {
+		map.set(c, map.has(c) ? false : true);
+	}
+	for (const item of map.keys()) {
+		if (map.get(item)) return item;
 	}
 	return ' ';
 };
