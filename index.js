fe69e07a544b75b49d22e025f541bd66bff41b90
刘可新 <liukexin08@maoyan.com>
2021-03-29 10:42:29 +0800

    二叉搜索树的第k大节点

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var search = function (nums, target) {
-	let i = 0;
-	let j = nums.length - 1;
-	while (i <= j) {
-		let m = Math.ceil((i + j) / 2);
-		if (nums[m] <= target) i = m + 1;
-		else j = m - 1;
+var kthLargest = function (root, k) {
+	let arr = [root];
+	function getval(head) {
+		if (head === null) return;
+		if (head.right !== null) arr.push(head.right);
+		if (head.left !== null) arr.push(head.left);
+		getval(head.left);
+		getval(head.right);
 	}
-	let right = i;
-	i = 0;
-	j = nums.length - 1;
-	while (i <= j) {
-		let m = Math.ceil((i + j) / 2);
-		if (nums[m] < target) i = m + 1;
-		else j = m - 1;
-	}
-	let left = j;
-	return right - left - 1;
+	getval(root);
+	arr.sort(function (a, b) {
+		return b.val - a.val;
+	});
+	return arr[k - 1].val;
 };
