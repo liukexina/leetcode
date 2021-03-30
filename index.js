9b343d5a4c2d9bc593114593786bae00623c21d0
刘可新 <liukexin08@maoyan.com>
2021-03-30 10:58:29 +0800

    二叉树的深度

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+let res, k;
+
 var kthLargest = function (root, k) {
-	let arr = [root];
-	function getval(head) {
-		if (head === null) return;
-		if (head.right !== null) arr.push(head.right);
-		if (head.left !== null) arr.push(head.left);
-		getval(head.left);
-		getval(head.right);
+	function dfs(root) {
+		if (root == null) return;
+		dfs(root.right);
+		if (k == 0) return;
+		if (--k == 0) res = root.val;
+		dfs(root.left);
 	}
-	getval(root);
-	arr.sort(function (a, b) {
-		return b.val - a.val;
-	});
-	return arr[k - 1].val;
+
+	this.k = k;
+	dfs(root);
+	return res;
 };
