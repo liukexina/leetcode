02af320d32e846c33d4f2a998a676451c81d80d0
刘可新 <liukexin08@maoyan.com>
2021-04-01 20:10:57 +0800

    翻转单词顺序

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-var isBalanced = function (root) {
-	return recur(root) != -1;
+var reverseWords = function (s) {
+	return s.trim().split(' ').reverse().join(' ').replace(/\s+/g, ' ');
 };
-
-function rect(root) {
-	if (root === null) return 0;
-	let left = rect(root.left);
-	if (left === -1) return -1;
-	let right = rect(root.right);
-	if (right === -1) return -1;
-	return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
-}
