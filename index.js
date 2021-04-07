ac2e4c258c656937fd990e0c2c1f1ada7ed6cda4
刘可新 <liukexin08@maoyan.com>
2021-04-07 21:18:02 +0800

    二叉搜索树的最近公共祖先

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var lastRemaining = function (n, m) {
-	// let arr = [];
-	// let i;
-	// for (i = 0; i < n; i++) {
-	//     arr.push(i)
+var lowestCommonAncestor = function (root, p, q) {
+	// while (root != null) {
+	//     if (root.val < p.val && root.val < q.val) // p,q 都在 root 的右子树中
+	//         root = root.right; // 遍历至右子节点
+	//     else if (root.val > p.val && root.val > q.val) // p,q 都在 root 的左子树中
+	//         root = root.left; // 遍历至左子节点
+	//     else break;
 	// }
-	// i = 0;
-	// while (arr.length !== 1) {
-	//     i = (i + m - 1) % arr.length;
-	//     arr.splice(i, 1)
+	// return root;
+	// if (p.val > q.val) { // 保证 p.val < q.val
+	//     let tmp = p;
+	//     p = q;
+	//     q = tmp;
 	// }
-	// return arr[0]
-
-	let ans = 0;
-	// 最后一轮剩下2个人，所以从2开始反推
-	for (let i = 2; i <= n; i++) {
-		ans = (ans + m) % i;
-	}
-	return ans;
+	// while (root != null) {
+	//     if (root.val < p.val) // p,q 都在 root 的右子树中
+	//         root = root.right; // 遍历至右子节点
+	//     else if (root.val > q.val) // p,q 都在 root 的左子树中
+	//         root = root.left; // 遍历至左子节点
+	//     else break;
+	// }
+	// return root;
+	if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+	if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+	return root;
 };
