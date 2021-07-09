db0fc2ed435d42ea838ddc907c34219700671d7d
刘可新 <liukexin08@maoyan.com>
2021-07-09 11:44:18 +0800

    45-反转字符串

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-var shortestToChar = function (s, c) {
-	// const sArr = s.split('')
-	// const result = []
-	// const indexArr = [...sArr.entries()]
-	//   .filter(([, x]) => x === c)
-	//   .map(([idx]) => idx)
-	// for (const [index, iterator] of sArr.entries()) {
-	//   const l = indexArr
-	//     .map((item) => Math.abs(index - item))
-	//     .sort((a, b) => a - b)
-	//   result.push(l[0])
-	// }
-	// return result
-	let N = s.length;
-	let ans = new Array(N);
-	let prev = -10400;
-
-	for (let i = 0; i < N; ++i) {
-		if (s[i] == c) prev = i;
-		ans[i] = i - prev;
+var reverseString = function (s) {
+	let t;
+	let i = 0;
+	let length = s.length;
+	while (i < length / 2) {
+		t = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = t;
+		i++;
 	}
-
-	prev = 10400;
-	for (let i = N - 1; i >= 0; --i) {
-		if (s[i] == c) prev = i;
-		ans[i] = Math.min(ans[i], prev - i);
-	}
-
-	return ans;
+	return s;
 };
