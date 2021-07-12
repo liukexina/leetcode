bc4f62e1eb533c4aab5115dc9c42084a40b065c8
刘可新 <liukexin08@maoyan.com>
2021-07-12 12:53:55 +0800

    判断质数、回文数

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var reverseString = function (s) {
-	let t;
-	let i = 0;
-	let length = s.length;
-	while (i < length / 2) {
-		t = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = t;
-		i++;
+function isPrime(num) {
+	if (num <= 3) {
+		return num > 1;
 	}
-	return s;
-};
+	// 不在6的倍数两侧的一定不是质数
+	if (num % 6 != 1 && num % 6 != 5) {
+		return false;
+	}
+	let sqrt = Math.sqrt(num);
+	for (let i = 5; i <= sqrt; i += 6) {
+		if (num % i == 0 || num % (i + 2) == 0) {
+			return false;
+		}
+	}
+	return true;
+}
