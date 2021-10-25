e9ede28947a9c49ae31f66d417f0ee40ccd28e38
刘可新 <liukexin08@maoyan.com>
2021-10-25 11:57:19 +0800

    在排序数组中查找数字

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function cuttingRope(n) {
-  let maxnum = 1;
-	// start 开始段
-	// temp 乘积
-	// n 剩余段
-  function dfs(start, temp, n) {
-    if (n == 1 || n == 0) {
-      maxnum = Math.max(maxnum, temp);
+var search = function (nums, target) {
+  function getRight(target) {
+    let i = 0;
+    let j = nums.length - 1;
+    // 右
+    while(i <= j) {
+      let r = Math.floor((i+j)/2);
+      if(nums[r] <= target) {
+        i = r + 1;
+      } else {
+        j = r - 1;
+      }
     }
-
-    for (let i = 0; i < n - 1; i++) {
-      dfs(start + i, temp * (start + i), n - start - i);
-    }
+    return i;
   }
-  dfs(1, 1, n);
-  return maxnum;
-}
+  return getRight(target) - getRight(target-1)
+};
