f55cd3619280c69d4957424f186793e4a3d5c687
刘可新 <liukexin08@maoyan.com>
2021-11-01 12:53:50 +0800

    礼物的最大价值

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var maxProfit = function (prices) {
-  if (prices.length === 0) return 0;
-  const dp = [0];
-  let min = prices[0];
-  for (let i = 1; i < prices.length; i++) {
-    min = Math.min(min, prices[i - 1]);
-    dp[i] = Math.max(dp[i - 1], prices[i] - min);
+var maxValue = function(grid) {
+  const sum = [[]];
+  sum[0][0] = grid[0][0]
+  const n = grid[0].length;
+  const m = grid.length;
+  for(let i = 1 ; i < n ; i++) {
+    sum[0][i] = sum[0][i-1] + grid[0][i];
   }
-  return dp[prices.length - 1];
+  for (let i = 1 ; i < m ; i++) {
+    sum[i][0] = sum[i-1][0] + grid[i][0];
+  }
+  for(let i = 1 ; i < m ; i++) {
+    for(let j = 1; j < n ; j++) {
+      sum[i][j] += Math.max(sum[i-1][j], sum[i][j-1]) + grid[i][j]
+    }
+  }
+  return sum[m-1][n-1]
 };
