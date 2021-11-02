abc4c69e327cf0494beb2b2aa98239ec04371dc5
刘可新 <liukexin08@maoyan.com>
2021-11-02 13:41:07 +0800

    最长不含重复字符的子字符串、把数字翻译成字符串

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-var maxValue = function(grid) {
-  const sum = [[]];
-  sum[0][0] = grid[0][0]
-  const n = grid[0].length;
-  const m = grid.length;
-  for(let i = 1 ; i < n ; i++) {
-    sum[0][i] = sum[0][i-1] + grid[0][i];
+var lengthOfLongestSubstring = function(s) {
+  let dic = new Map();
+  let i = -1, res = 0;
+  for(let j = 0; j < s.length; j++) {
+      if(dic.get(s[j]) !== undefined) {
+        i = Math.max(i, dic.get(s[j])); // 更新左指针 i
+      }
+      dic.set(s[j], j); // 哈希表记录
+      res = Math.max(res, j - i); // 更新结果
   }
-  for (let i = 1 ; i < m ; i++) {
-    sum[i][0] = sum[i-1][0] + grid[i][0];
-  }
-  for(let i = 1 ; i < m ; i++) {
-    for(let j = 1; j < n ; j++) {
-      sum[i][j] += Math.max(sum[i-1][j], sum[i][j-1]) + grid[i][j]
-    }
-  }
-  return sum[m-1][n-1]
-};
+  return res;
+}
