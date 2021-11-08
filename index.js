a2a3aca641ae0800ba541dd0c9601af8fa00dada
刘可新 <liukexin08@maoyan.com>
2021-11-08 14:14:18 +0800

    二叉搜索树与双向链表\二叉树中和为某一值的路径

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var twoSum = function (nums, target) {
-  let i = 0;
-  let j = nums.length - 1;
-  while(i < j) {
-    let cha = target - nums[i];
-    if(nums[j] > cha) {
-      j--
-    } else if(nums[j] < cha) {
-      i++;
-    } else {
-      break;
-    }
-  }
-  return [nums[i],nums[j]]
-}
+var treeToDoublyList = function(root) {
+  let index = 0;
+        dfs(root);
+        
+        while(index < arr.length) {
+          arr[index].left = index !== 0 ? arr[index-1] : arr[arr.length - 1];
+          arr[index].right = index !== arr.length - 1 ? arr[index+1] : arr[0];
+          index++;
+        }
+        return arr[0];
+        function dfs(root) {
+          if(root === null) return;
+          dfs(root.left)
+          arr.push(root)
+          dfs(root.right)
+        }
+};
