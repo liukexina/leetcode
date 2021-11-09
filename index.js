eed332333ac07813280112e3fbb7a32831527283
刘可新 <liukexin08@maoyan.com>
2021-11-09 11:46:33 +0800

    把数组排成最小的数

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var treeToDoublyList = function(root) {
+var treeToDoublyList = function (root) {
   let index = 0;
-        dfs(root);
-        
-        while(index < arr.length) {
-          arr[index].left = index !== 0 ? arr[index-1] : arr[arr.length - 1];
-          arr[index].right = index !== arr.length - 1 ? arr[index+1] : arr[0];
-          index++;
-        }
-        return arr[0];
-        function dfs(root) {
-          if(root === null) return;
-          dfs(root.left)
-          arr.push(root)
-          dfs(root.right)
-        }
+  dfs(root);
+
+  while (index < arr.length) {
+    arr[index].left = index !== 0 ? arr[index - 1] : arr[arr.length - 1];
+    arr[index].right = index !== arr.length - 1 ? arr[index + 1] : arr[0];
+    index++;
+  }
+  return arr[0];
+  function dfs(root) {
+    if (root === null) return;
+    dfs(root.left);
+    arr.push(root);
+    dfs(root.right);
+  }
 };
