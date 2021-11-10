11125bd54059f8d31d4256bda9ff4fc6a9e68f6a
刘可新 <liukexin08@maoyan.com>
2021-11-10 13:11:09 +0800

    数据流中的中位数

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-var treeToDoublyList = function (root) {
-  let index = 0;
-  dfs(root);
+/**
+ * initialize your data structure here.
+ */
+var MedianFinder = function () {
+  this.arr = [];
+};
 
-  while (index < arr.length) {
-    arr[index].left = index !== 0 ? arr[index - 1] : arr[arr.length - 1];
-    arr[index].right = index !== arr.length - 1 ? arr[index + 1] : arr[0];
-    index++;
-  }
-  return arr[0];
-  function dfs(root) {
-    if (root === null) return;
-    dfs(root.left);
-    arr.push(root);
-    dfs(root.right);
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+  sortN(this.arr, num);
+  function sortN(arr, num) {
+    low = 0;
+    high = arr.length - 1;
+
+    while (low <= high) {
+      m = Math.floor((low + high) / 2);
+      if (arr[m] > num) high = m - 1;
+      else low = m + 1;
+    }
+
+    if (low === 0) {
+      arr.unshift(num);
+    } else {
+      arr.splice(low, 0, num);
+    }
   }
 };
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+  let middle = (this.arr.length - 1) / 2;
+  if (Number.isInteger(middle)) {
+    return this.arr[middle];
+  } else {
+    return (this.arr[Math.ceil(middle)] + this.arr[Math.floor(middle)]) / 2;
+  }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
