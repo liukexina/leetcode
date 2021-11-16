809a19c29cd181be338f658066f3c5e6a7958356
刘可新 <liukexin08@maoyan.com>
2021-11-16 11:07:39 +0800

    二进制中1的个数\不用加减乘除做加法

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-var verifyPostorder = function(postorder) {
-  if(postorder.length === 0) return true;
-  return dfs(0, postorder.length-1);
+var verifyPostorder = function (postorder) {
+  if (postorder.length === 0) return true;
+  return dfs(0, postorder.length - 1);
 
-  function dfs(start ,end) {
+  function dfs(start, end) {
     let i;
     let j;
-    if(start >= end) return true;
-   
-      i = start;
-      while(postorder[i] <= postorder[end] ) i++;
-      j = i;
-      while(postorder[j] >= postorder[end] ) j++;
-      return j === end && dfs(start, i-1) & dfs(i,end-1)
-    
-  }
+    if (start >= end) return true;
 
+    i = start;
+    while (postorder[i] <= postorder[end]) i++;
+    j = i;
+    while (postorder[j] >= postorder[end]) j++;
+    return j === end && dfs(start, i - 1) & dfs(i, end - 1);
+  }
 };
