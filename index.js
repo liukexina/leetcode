04548f9f7382970600cb579d6146ad1d1f423cb4
刘可新 <liukexin08@maoyan.com>
2021-11-17 13:12:39 +0800

    数组中数字出现的次数

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var verifyPostorder = function (postorder) {
-  if (postorder.length === 0) return true;
-  return dfs(0, postorder.length - 1);
-
-  function dfs(start, end) {
-    let i;
-    let j;
-    if (start >= end) return true;
-
-    i = start;
-    while (postorder[i] <= postorder[end]) i++;
-    j = i;
-    while (postorder[j] >= postorder[end]) j++;
-    return j === end && dfs(start, i - 1) & dfs(i, end - 1);
+var singleNumbers = function(nums) {
+  let value = 0;
+  for(let i = 0 ; i < nums.length ; i++) {
+    value ^= nums[i]
   }
+  return value;
 };
