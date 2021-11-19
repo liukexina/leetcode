fe0f15aa12151d086a337ac9e07baebc3beddd4d
刘可新 <liukexin08@maoyan.com>
2021-11-19 10:00:51 +0800

    剪绳子\构建乘积数组

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var singleNumbers = function(nums) {
-  let value = 0;
-  for(let i = 0 ; i < nums.length ; i++) {
-    value ^= nums[i]
-  }
-  return value;
-};
+function cuttingRope( n) {
+  if(n <= 3) return n - 1;
+  let a = Math.floor(n / 3);
+  let b = n % 3;
+  if(b == 0) return Math.pow(3, a);
+  if(b == 1) return Math.pow(3, a - 1) * 4;
+  return Math.pow(3, a) * 2;
+}
