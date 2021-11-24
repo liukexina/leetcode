46222d4ed844e1d7a5bda6ec8550d8881d4540a6
刘可新 <liukexin08@maoyan.com>
2021-11-24 19:56:41 +0800

    表示数值的字符串\把字符串转换成整数

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var validateStackSequences = function(pushed, popped) {
-  const arr = [];
-  let i = 0;
-  for (const iterator of pushed) {
-    arr.push(iterator);
-    while(arr.length && arr[arr.length-1] === popped[i]) {
-      arr.pop();
-      i++;
-    }
+function isNumber(s) {
+  let states = [
+      { ' ': 0, 's': 1, 'd': 2, '.': 4 }, 
+      { 'd': 2, '.': 4 } ,                
+      { 'd': 2, '.': 3, 'e': 5, ' ': 8 }, 
+      { 'd': 3, 'e': 5, ' ': 8 },         
+      { 'd': 3 },                        
+      { 's': 6, 'd': 7 },                 
+      { 'd': 7 },                         
+      { 'd': 7, ' ': 8 },                 
+      { ' ': 8 }                          
+  ]
+  let p = 0;
+  let t;
+  for(let c of s.split('')) {
+      if(c >= '0' && c <= '9') t = 'd';
+      else if(c == '+' || c == '-') t = 's';
+      else if(c == 'e' || c == 'E') t = 'e';
+      else if(c == '.' || c == ' ') t = c;
+      else t = '?';
+      if(!states[p].hasOwnProperty(t)) return false;
+      p = states[p][t];
   }
-  return !arr.length
-};
+  return p == 2 || p == 3 || p == 7 || p == 8;
+}
