12e75c95e6360830b91c48ab7a801aef428f42bd
刘可新 <liukexin08@maoyan.com>
2021-11-29 12:04:29 +0800

    正则表达式匹配、丑数、n个骰子的点数

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var permutation = function(s) {
-        let result = [];
-        for (const iterator of s.split('')) {
-          let cur = [];
-          if(result.length === 0) {
-            result.push(iterator);
-            continue;
-          }
-          for (const item of result) {
-            for(let i = 0 ; i <= item.length ; i++) {
-              let value = item.split('');
-              value.splice(i,0,iterator);
-              if(Array.from(new Set(value)).length === value.length) cur.push(value.join(''))
-            }
-          }
-          result = Array.from(new Set(cur));
-        }
-        return result
-      };
+function dicesProbability(n) {
+  let dp = new Array(6).fill(1 / 6);
+  for (let i = 2; i <= n; i++) {
+    let tmp = new Array(5 * i + 1).fill(0);
+    for (let j = 0; j < dp.length; j++) {
+      for (let k = 0; k < 6; k++) {
+        tmp[j + k] += dp[j] / 6.0;
+      }
+    }
+    dp = tmp;
+  }
+  return dp;
+}
