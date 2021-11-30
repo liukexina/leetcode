a76fa8341cdd8aa0760c338d314b80dca63c9c7d
刘可新 <liukexin08@maoyan.com>
2021-11-30 11:50:25 +0800

    数组中的逆序对、打印从1到最大的n位数

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-function dicesProbability(n) {
-  let dp = new Array(6).fill(1 / 6);
-  for (let i = 2; i <= n; i++) {
-    let tmp = new Array(5 * i + 1).fill(0);
-    for (let j = 0; j < dp.length; j++) {
-      for (let k = 0; k < 6; k++) {
-        tmp[j + k] += dp[j] / 6.0;
+function reversePairs(nums) {
+  let tmp = new Array(nums.length);
+  return mergeSort(0, nums.length - 1);
+
+  function mergeSort(l, r) {
+    // 终止条件
+    if (l >= r) return 0;
+    // 递归划分
+    let m = Math.floor((l + r) / 2);
+    let res = mergeSort(l, m) + mergeSort(m + 1, r);
+    // 合并阶段
+    let i = l,
+      j = m + 1;
+    for (let k = l; k <= r; k++) tmp[k] = nums[k];
+    for (let k = l; k <= r; k++) {
+      if (i == m + 1) nums[k] = tmp[j++];
+      else if (j == r + 1 || tmp[i] <= tmp[j]) nums[k] = tmp[i++];
+      else {
+        nums[k] = tmp[j++];
+        res += m - i + 1; // 统计逆序对
       }
     }
-    dp = tmp;
+    return res;
   }
-  return dp;
 }
