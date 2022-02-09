473d53d4d006e874c607b3df861b54fdcf28cbb2
刘可新 <liukexin08@maoyan.com>
2022-02-09 17:10:39 +0800

    三数之和\盛最多水的容器\正则表达式匹配

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-var longestPalindrome = function (s) {
-  let result = "";
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      let str = s.substring(i, j);
-      if (
-        str === str.split("").reverse().join("") &&
-        str.length > result.length
-      ) {
-        result = str;
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let arr = [];
+  for (let k = 0; k < nums.length - 2; k++) {
+    if (nums[k] > 0) break;
+    if (k > 0 && nums[k - 1] === nums[k]) continue;
+    let i = k + 1;
+    let j = nums.length - 1;
+    let target = 0 - nums[k];
+    while (i < j) {
+      sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        arr.push([nums[k], nums[i], nums[j]]);
+        while (i < j && nums[i] == nums[i + 1]) i++;
+        while (i < j && nums[j] == nums[j - 1]) j--;
+        i++;
+        j--;
+      } else if (sum < 0) {
+        i++;
+      } else {
+        j--;
       }
     }
   }
-  return result;
+
+  return arr;
 };
