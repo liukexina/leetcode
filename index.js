f42b9a5ef286504be499fb76c8499d1499c61212
刘可新 <liukexin08@maoyan.com>
2022-02-10 20:06:03 +0800

    有效的括号

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-var threeSum = function (nums) {
-  nums.sort((a, b) => a - b);
-  let arr = [];
-  for (let k = 0; k < nums.length - 2; k++) {
-    if (nums[k] > 0) break;
-    if (k > 0 && nums[k - 1] === nums[k]) continue;
-    let i = k + 1;
-    let j = nums.length - 1;
-    let target = 0 - nums[k];
-    while (i < j) {
-      sum = nums[i] + nums[j] + nums[k];
-      if (sum === 0) {
-        arr.push([nums[k], nums[i], nums[j]]);
-        while (i < j && nums[i] == nums[i + 1]) i++;
-        while (i < j && nums[j] == nums[j - 1]) j--;
-        i++;
-        j--;
-      } else if (sum < 0) {
-        i++;
-      } else {
-        j--;
-      }
+var isValid = function (s) {
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "["
+  };
+
+  const arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      if (arr[0] !== map[s[i]]) return false;
+      arr.shift();
+    } else {
+      arr.unshift(s[i]);
     }
   }
 
-  return arr;
+  return !arr.length;
 };
