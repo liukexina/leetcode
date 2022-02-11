935da9720f8d80c3198b1a234fbba2084bbae71f
刘可新 <liukexin08@maoyan.com>
2022-02-11 20:09:50 +0800

    搜索旋转排序数组

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-var isValid = function (s) {
-  const map = {
-    ")": "(",
-    "}": "{",
-    "]": "["
-  };
-
-  const arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (map[s[i]]) {
-      if (arr[0] !== map[s[i]]) return false;
-      arr.shift();
+var search = function (nums, target) {
+  let n = nums.length;
+  if (n == 0) return -1;
+  if (n == 1) return nums[0] == target ? 0 : -1;
+  let l = 0;
+  let r = n - 1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    }
+    if (nums[0] <= nums[mid]) {
+      if (nums[0] <= target && target < nums[mid]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
     } else {
-      arr.unshift(s[i]);
+      if (nums[mid] < target && target <= nums[n - 1]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
   }
-
-  return !arr.length;
+  return -1;
 };
