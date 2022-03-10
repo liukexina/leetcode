f2084a771d40862f74bae4280017513d2d40dd60
刘可新 <liukexin08@maoyan.com>
2022-03-10 16:11:37 +0800

    合并区间

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var canJump = function (nums) {
-  let n = nums.length;
-  let rightmost = 0;
-  for (let i = 0; i < n; ++i) {
-    if (i <= rightmost) {
-      rightmost = Math.max(rightmost, i + nums[i]);
-      if (rightmost >= n - 1) {
-        return true;
-      }
+var merge = function (intervals) {
+  if (intervals.length === 1) return intervals;
+  intervals.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  let ans = [];
+  for (let i = 0; i < intervals.length; ) {
+    let t = intervals[i][1];
+    let j = i + 1;
+    while (j < intervals.length && intervals[j][0] <= t) {
+      t = Math.max(t, intervals[j][1]);
+      j++;
     }
+    ans.push([intervals[i][0], t]);
+    i = j;
   }
-  return false;
+  return ans;
 };
